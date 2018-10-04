9829539a78d9e978622a1de3ca12df5eae377fb6
Aflamedesert <Michael.w.crowe@gmail.com>
2018-10-03 23:35:18 -0700

    Second Grumpy Bot

--- main.cpp
+++ main.cpp
@@ -10,25 +10,26 @@ bool notContain(string sentence, string query);
 int main(){
     cout << "Ugh. Today's a bad day";
     bool terminator = true;
-    string userQuestion;
     while (terminator){
-        cin >> userQuestion;
+        string userQuestion;
+        getline(cin, userQuestion);
         if(contains(userQuestion, "!")){
                 cout << "Calm Down, buddy.";
         }
         else if(contains(userQuestion, "?")){
                 cout << "What do you think?!";
-                cin >> userQuestion;
+                userQuestion = "";
+                getline(cin, userQuestion);
                 if((userQuestion.find("how")!=string::npos) || (userQuestion.find("why")!=string::npos)){
                         cout << "Enough with the questions";
                 }
         }
-        else if((notContain(userQuestion, "!")) || (notContain(userQuestion, "?")) || (notContain(userQuestion, "."))){
-                cout << "Didn't you learn punctuation in middle school?";
-        }
         else if((userQuestion.find("bye") != string::npos) || (userQuestion.find("Bye") != string::npos)){
                 cout << "Finally some peace";
                 terminator = false;
+        }
+        else if(!((userQuestion.find("!") != string::npos) || (userQuestion.find("?") != string::npos) || (userQuestion.find(".") != string::npos))){
+                cout << "Didn't you learn punctuation in elementary school?";
         }
         else{
                 cout << "Sigh...";
